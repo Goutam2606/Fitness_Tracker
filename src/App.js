93a2da535bc3888446e8dd0d03954d146cdaa0fa
Goutam2606 <goutamy.bca2022@ssism.org>
2024-09-04 13:55:57 +0530

    Update project files and add new components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { ThemeProvider, styled } from 'styled-components';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { lightTheme } from './utils/Themes';
+import Authentication from './pages/Authentication';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import Dashboard from './pages/Dashboard';
+import Workouts from './pages/Workouts';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 
 function App() {
+  const [user, setUser] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        {user ? (
+          <Container>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/workouts' element={<Workouts />} />
+            </Routes>
+          </Container>
+        ) : (
+          <Container>
+            <Authentication />
+          </Container>
+        )}
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
